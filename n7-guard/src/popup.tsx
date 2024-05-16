b2cc00ad8f7606ddbd200ada40897cfb344d77eb
anaserrami <anaserrami24@gmail.com>
2024-05-16 13:05:03 +0100

    add ext frontend

--- n7-guard/src/popup.tsx
+++ n7-guard/src/popup.tsx
@@ -1,13 +1,23 @@
-import { CountButton } from "~features/count-button"
-
 import "~style.css"
+import redmark from "data-base64:~../assets/redmark.png"
+import check from "data-base64:~../assets/check.png"
 
 function IndexPopup() {
+  const isPhishing = true;
+
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div className="p-8 text-center">
+      <img
+        src={isPhishing ? redmark : check}
+        alt={isPhishing ? "Phishing" : "Safe"}
+        className="inline-block w-16 h-16 mx-auto mb-4"
+      />
+      
+      <div className={isPhishing ? "text-red-500 text-lg" : "text-green-500 text-lg"}>
+        {isPhishing ? "This website is phishing." : "This website is safe."}
+      </div>
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
