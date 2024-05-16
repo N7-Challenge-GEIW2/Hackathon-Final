7eb26e57126f9c23a321397a1a1eb9cb48ad922f
anaserrami <anaserrami24@gmail.com>
2024-05-16 13:46:27 +0100

    add ext frontend

--- n7-guard/src/popup.tsx
+++ n7-guard/src/popup.tsx
@@ -1,4 +1,3 @@
-import { CountButton } from "~features/count-button"
 import React from "react"
 import "~style.css"
 import redmark from "data-base64:~../assets/redmark.png"
@@ -9,16 +8,20 @@ import check from "data-base64:~../assets/check.png"
 function IndexPopup() {
   const isPhishing = true;
   return (
-    <div className="p-8 text-center">
+    <div className="w-64 p-8 text-center">
       <img
         src={isPhishing ? redmark : check}
         alt={isPhishing ? "Phishing" : "Safe"}
         className="inline-block w-16 h-16 mx-auto mb-4"
       />
       
-      <div className={isPhishing ? "text-red-500 text-lg" : "text-green-500 text-lg"}>
+      <div className={isPhishing ? "text-lg font-bold" : "text-lg font-bold"}>
         {isPhishing ? "This website is phishing." : "This website is safe."}
       </div>
+
+      <button className="px-2 py-1 mt-4 font-bold text-blue-500 bg-white border border-blue-500 rounded hover:bg-blue-500 hover:text-white">
+        Dashboard
+      </button>
     </div>
   );
 }
